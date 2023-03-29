66c41902bb568728ce3d8ec6cfcc6f84a2b2f5ab
arthurariza <tucaariza@gmail.com>
2023-03-29 19:51:36 -0300

    Job Chaining https://laravel.com/docs/master/queues#job-chaining

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Jobs\ChainJob;
 use App\Jobs\PriorityJob;
 use App\Jobs\SendWelcomeEmailJob;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,4 +31,22 @@ Route::get('/fail', function () {
     PriorityJob::dispatch();
 
     return view('welcome');
+});
+
+Route::get('/chain', function () {
+    // Job chaining allows you to specify a list of queued jobs that should be run in sequence
+    Bus::chain([
+        new ChainJob(),
+        new ChainJob(),
+        new ChainJob(),
+    ])->onQueue('high')->dispatch();
+
+    // If one job in the sequence fails, the rest of the jobs will not be run.
+    Bus::chain([
+        new ChainJob(true),
+        new ChainJob(),
+        new ChainJob(),
+    ])->onQueue('high')->dispatch();
+
+   return 'Chained';
 });
